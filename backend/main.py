c1b33a65a3c874e45b358d4463dc7a216f857dcf
CameronEdw03 <camedwards@Cams-MacBook-Air.local>
2025-05-26 11:26:49 -0500

    commit backend change

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
         "http://localhost:5174",    
         "http://localhost:5175",    
         "http://localhost:8080", 
-        "https://coffee-log-mu.vercel.app/"  
+        "https://coffee-log-mu.vercel.app"  
     ],
     allow_credentials=True,
     allow_methods=["*"],
